0a7b118659eaa9ac9d55cf2b293467c473d0e49f
egor.eichhorn <eichhornegor@gmail.com>
2024-04-23 16:26:07 +0600

    Updates

---
+++ assets/index--IdB2OB9.js
@@ -0,0 +1,1 @@
+import{b as o,j as n}from"./index-Bwx5TvpJ.js";function s(){const{l:t}=o();return n.jsx("h1",{children:t.notFound.title})}export{s as default};
--- assets/index-DXsqySlh.js
+++
@@ -1,1 +0,0 @@
-import{b as o,j as n}from"./index-CPZEJP3R.js";function s(){const{l:t}=o();return n.jsx("h1",{children:t.notFound.title})}export{s as default};
